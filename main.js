68b5d5480615d46e1b0cd08bd37288395fdce721
Jade Wee <jadeweeee@gmail.com>
2018-07-18 12:10:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ $(document).ready(function() {
     // BIZ_MANAGER_ID logic
 
     $.ajax({
-      url: "https://graph.facebook.com/v2.9/" + account_id + "/customaudiences",
+      url: "https://graph.facebook.com/v2.11/" + account_id + "/customaudiences",
       type: 'POST',
       dataType: 'json',
       data: formData,
